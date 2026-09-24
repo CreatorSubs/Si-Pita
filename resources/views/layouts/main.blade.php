6a0236b5f51da61222e7c42f23e059a481abf35a
Abhi Abu / Tabu <gamerabhi124@gmail.com>
2026-09-24 08:24:42 +0700

    please

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield<a href="{{ url('/') }}">
-    <img src="{{ asset('images/diskominfo.webp') }}" alt="Logo Diskominfo" style="height: 40px; width: auto;">
-</a></title>
+    <title>Si-Pita</title>
     
+    <a href="{{ url('/') }}">
+        <img src="{{ asset('images/diskominfo.webp') }}" alt="Logo Diskominfo" style="height: 40px; width: auto;">
+    </a>
+
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
